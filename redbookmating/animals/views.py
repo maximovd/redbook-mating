bd9cf335ceadcc189d6f7d942f38d1ac452e8f1a
Danil Maximov <bazinga.mail@yandex.ru>
2021-09-28 17:17:19 +0300

    Add animals nearby methods

--- redbookmating/animals/views.py
+++ redbookmating/animals/views.py
@@ -1,5 +1,10 @@
+from django.contrib.gis import measure
+from rest_framework import status
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+import settings
 from animals.models import AnimalType, AnimalProperty, Animal
 from animals.serializers import AnimalTypeSerializer, AnimalPropertySerializer, AnimalSerializer
 
@@ -20,3 +25,19 @@ class AnimalViewSet(viewsets.ModelViewSet):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=True, methods=['get'])
+    def get_nearby(self, request, pk=None) -> Response:
+        """Get nearby animals over target animal."""
+        km = request.data.get('km', settings.DEFAULT_SEARCH_DISTANCE)
+        try:
+            found_animal = Animal.objects.get(id=pk)
+        except (Animal.DoesNotExist, Animal.MultipleObjectsReturned):
+            return Response({'text': 'Animal does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        distance_from_point = {'km': km}
+        nearby_animals = Animal.objects.filter(
+            location__distance_lte=(found_animal.location, measure.D(**distance_from_point)),
+            type=found_animal.type,
+        ).exclude(id=pk)
+        nearby_animals_serializable = AnimalSerializer(nearby_animals, many=True)
+        return Response(nearby_animals_serializable.data, status=status.HTTP_200_OK)
--- redbookmating/redbookmating/urls.py
+++ redbookmating/redbookmating/urls.py
@@ -54,6 +54,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
+
     # Docs
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
